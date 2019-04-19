ef6f0de0923fc44aa4d9459e19799bb2d99e06d6
Sergei Hanka <sergeiahanka@gmail.com>
2019-04-19 16:29:51 -0500

    updated CORS policy

--- src/main/java/main/RewardsController.java
+++ src/main/java/main/RewardsController.java
@@ -3,15 +3,15 @@ package main;
 import java.io.IOException;
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import domain.RewardItem;
 import domain.RewardLoader;
 
 @RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RequestMapping("/rewards")
 public class RewardsController {
 	
