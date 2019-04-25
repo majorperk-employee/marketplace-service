dbc603b508ea4f06b2909929b982cb9de1225766
Sergei Hanka <sergeiahanka@gmail.com>
2019-04-25 18:37:48 -0500

    removed example

--- src/main/java/com/majorperk/marketservice/repository/example/QuestionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.majorperk.marketservice.repository.example;
-
-import com.majorperk.marketservice.model.example.Question;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface QuestionRepository extends JpaRepository<Question, Long> {
-}
