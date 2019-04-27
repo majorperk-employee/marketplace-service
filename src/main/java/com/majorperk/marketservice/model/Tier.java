ff4eb685d29b5e010abaeb7a03fa498caf8aab9a
Luke Snell <ljsnell7@gmail.com>
2019-04-27 02:13:52 -0500

    Couple of questions still to be answered regarding requirements but the framework is inplace.
    Signed-off-by: Luke Snell <ljsnell7@gmail.com>

--- src/main/java/com/majorperk/marketservice/model/Tier.java
+++ src/main/java/com/majorperk/marketservice/model/Tier.java
@@ -7,8 +7,37 @@ public class Tier {
 	String currentTier;
 	String nextTier;	
 	
-	int onTimeGoal;
+	double onTimeGoal;
 	int totalDaysGoal;
-	// Create factory for tiers. Accept user with tier and run logic on it
-	// to determine their tier.
+
+	public String getCurrentTier() {
+		return currentTier;
+	}
+	public void setCurrentTier(String currentTier) {
+		this.currentTier = currentTier;
+	}
+	
+	public String getNextTier() {
+		return nextTier;
+	}
+	
+	public void setNextTier(String nextTier) {
+		this.nextTier = nextTier;
+	}
+	
+	public double getOnTimeGoal() {
+		return onTimeGoal;
+	}
+	
+	public void setOnTimeGoal(double onTimeGoal) {
+		this.onTimeGoal = onTimeGoal;
+	}
+	
+	public int getTotalDaysGoal() {
+		return totalDaysGoal;
+	}
+	
+	public void setTotalDaysGoal(int totalDaysGoal) {
+		this.totalDaysGoal = totalDaysGoal;
+	}
 }
--- src/main/java/com/majorperk/marketservice/service/TierService.java
+++ src/main/java/com/majorperk/marketservice/service/TierService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 
 import com.majorperk.marketservice.model.Account;
+import com.majorperk.marketservice.model.Tier;
 
 @Service
 public class TierService {
@@ -22,17 +23,54 @@ public class TierService {
 			Next Tier,
 			Goals: (OTP, TDW)*/
 	
+	public final String PLATINUM = "Platinum";
+	public final String GOLD = "Gold";	
+	public final String SILVER = "Silver";
+	public final String EMPLOYEE = "Employee";
+	
+	public final double PLATINUM_PERCENT = .95;
+	public final double GOLD_PERCENT = .90;	
+	public final double SILVER_PERCENT = .85;
+	
+	public final int PLATINUM_DAYS = 120;
+	public final int GOLD_DAYS = 90;	
+	public final int SILVER_DAYS = 60;
+	
 	public List<Account> updateTiers(List<Account> accountsToUpdate) {
 		accountsToUpdate.forEach(account -> {
-			account = updateTier(account);
+			account.setTier(updateTier(account));
 		});
 		
 		return accountsToUpdate;		
 	}
 
-	private Account updateTier() {
-		// TODO Auto-generated method stub
-		return null;
+	private Tier updateTier(Account account) {
+		
+		int totalDays = account.getTotaldays();
+		double onTimePercent = account.getOntimedays() / totalDays;
+		Tier tier = account.getTier();
+		
+		// Should this be on time days (or maybe an && instead of ||) worked rather than just total days?
+		// Otherwise all they need to do is work there for 4 months & they're
+		// platinum, no matter how punctual they are.
+		if(totalDays > PLATINUM_DAYS || onTimePercent > PLATINUM_PERCENT) {
+			// What happens if they've reached max tier?
+			tier.setNextTier(PLATINUM);
+			tier.setCurrentTier(PLATINUM);
+			// Need to populate Goals. Is that total days worked - next tier's days?
+			// and current ontime percent - required ontime %?
+		} else if(totalDays > GOLD_DAYS || onTimePercent > GOLD_PERCENT) {
+			tier.setNextTier(PLATINUM);
+			tier.setCurrentTier(GOLD);
+		} else if(totalDays > SILVER_DAYS || onTimePercent > SILVER_PERCENT) {
+			tier.setNextTier(GOLD);
+			tier.setCurrentTier(SILVER);
+		} else {
+			tier.setNextTier(SILVER);
+			tier.setCurrentTier(EMPLOYEE);
+		}
+		
+		return tier;
 	}
 
 }
