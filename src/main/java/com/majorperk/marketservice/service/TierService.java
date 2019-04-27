7436f00d70bfe1e4477af3c200f832004dcc2cf5
Luke Snell <ljsnell7@gmail.com>
2019-04-26 22:35:19 -0500

    in progress
    Signed-off-by: Luke Snell <ljsnell7@gmail.com>

--- src/main/java/com/majorperk/marketservice/service/TierService.java
+++ src/main/java/com/majorperk/marketservice/service/TierService.java
@@ -2,17 +2,37 @@ package com.majorperk.marketservice.service;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.majorperk.marketservice.model.Account;
-import com.majorperk.marketservice.model.Cart;
-import com.majorperk.marketservice.model.RewardItem;
-import com.majorperk.marketservice.repository.AccountRepository;
-import com.majorperk.marketservice.repository.CartRepository;
-import com.majorperk.marketservice.repository.RewardRepository;
 
 @Service
 public class TierService {
+	
+	/*[Employee]
+			[Silver] if > 60 days worked || > 85% on time
+			[Gold] if > 90 days worked || > 90% on time
+			[Platinum] if > 120 days worked || > 95% on time
+
+			Input: User (totalDaysWorked, OnTimePercentage[tdw/totaldays])
+
+			Return Tier Object:
+
+			Current Tier,
+			Next Tier,
+			Goals: (OTP, TDW)*/
+	
+	public List<Account> updateTiers(List<Account> accountsToUpdate) {
+		accountsToUpdate.forEach(account -> {
+			account = updateTier(account);
+		});
+		
+		return accountsToUpdate;		
+	}
+
+	private Account updateTier() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
