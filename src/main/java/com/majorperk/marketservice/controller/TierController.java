83849271d8c1e12429873fea61d391b073e1af47
Luke Snell <ljsnell7@gmail.com>
2019-04-27 21:55:18 -0500

    Added endpoint to fetch tier by userId.
    Signed-off-by: Luke Snell <ljsnell7@gmail.com>

--- src/main/java/com/majorperk/marketservice/controller/TierController.java
+++ src/main/java/com/majorperk/marketservice/controller/TierController.java
@@ -1,12 +1,18 @@
 package com.majorperk.marketservice.controller;
 
+import com.majorperk.marketservice.model.Account;
+import com.majorperk.marketservice.model.Tier;
 import com.majorperk.marketservice.repository.AccountRepository;
 import com.majorperk.marketservice.service.TierService;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,8 +27,7 @@ class TierController {
 	
 	@Autowired
 	TierService tierService;
-	
-	// 
+
 	@ResponseBody
 	@RequestMapping(value = "/updateAllAccountTiers", method = RequestMethod.POST, produces = "application/json")
 	void updateAllAccountTiers() {
@@ -32,4 +37,14 @@ class TierController {
 			System.out.println("Unable to complete tier uppdate");
 		}
 	}
+	
+	  @GetMapping("/getTier")
+	  public Tier getTierById(@RequestParam(value = "userId", required = true) long userId) {
+		  try {
+			  return accountRepository.findById(userId).get().getTier();	  
+		  } catch (Exception e) {
+			  System.out.println();
+			  return new Tier();
+		  }	    
+	  }
 }
