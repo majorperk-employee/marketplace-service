cbfa6dd5b11587b0dd33d00ab0922f93b6d4cae7
Luke Snell <ljsnell7@gmail.com>
2019-05-19 19:28:42 -0500

    WIP for brand repository query.
    Signed-off-by: Luke Snell <ljsnell7@gmail.com>

--- src/main/java/com/majorperk/marketservice/controller/RewardLinkController.java
+++ src/main/java/com/majorperk/marketservice/controller/RewardLinkController.java
@@ -4,9 +4,6 @@ import java.util.List;
 
 import javax.validation.Valid;
 
-import com.majorperk.marketservice.model.reward.Brand;
-import com.majorperk.marketservice.service.TangoRewardService;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,6 +14,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.majorperk.marketservice.model.reward.Brand;
+import com.majorperk.marketservice.repository.BrandRepository;
+import com.majorperk.marketservice.service.TangoRewardService;
+
 @RestController
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RequestMapping("rewardlink")
@@ -25,13 +26,28 @@ class RewardLinkController {
 	@Autowired
 	private TangoRewardService tangoRewardService;
 
-    @GetMapping("/view")
-	public @Valid List<Brand> getCatalog() {
+	@Autowired
+	private BrandRepository brandRepository;
+
+	private static String brandKey = "B077325";
+	private static String rewardLinkText = "Reward Link Preferred";
+
+	@GetMapping("/view")
+	public @Valid Brand getRewardLinkItem() {
 		try {
-			return null; //RETURN REWARDLINK ITEM HERE
+			Brand brandKeyResponse = brandRepository.findOneByBrandKey(brandKey);
+			if (brandKeyResponse != null && brandKeyResponse.getBrandName().contains(rewardLinkText)) {
+				return brandKeyResponse;
+			}
+			Brand brandNameResponse = brandRepository.findOneByBrandName(rewardLinkText);
+			if (brandNameResponse != null) {
+				return brandNameResponse;
+			} else {
+				return new Brand();
+			}
 		} catch (Exception e) {
 			System.out.println("Unable to access TangoCard catalog :::" + e);
-			return null;
+			return new Brand();
 		}
 	}
 
@@ -46,16 +62,16 @@ class RewardLinkController {
 		}
 	}
 
-    // Checkout
-    @ResponseBody
-    @RequestMapping(value = "/redeem/{userid}", method = RequestMethod.POST, produces = "application/json")
-    Object redeemRewardLink(@PathVariable Long userid) {
-        try {
-            return tangoRewardService.redeemRewardLink(userid);
-        } catch (Exception e) {
-            System.out.println("Unable to complete rewardLink redemption for " + userid);
-            return null;
-        }
-    }
+	// Checkout
+	@ResponseBody
+	@RequestMapping(value = "/redeem/{userid}", method = RequestMethod.POST, produces = "application/json")
+	Object redeemRewardLink(@PathVariable Long userid) {
+		try {
+			return tangoRewardService.redeemRewardLink(userid);
+		} catch (Exception e) {
+			System.out.println("Unable to complete rewardLink redemption for " + userid);
+			return null;
+		}
+	}
 
 }
--- src/main/java/com/majorperk/marketservice/repository/BrandRepository.java
+++ src/main/java/com/majorperk/marketservice/repository/BrandRepository.java
@@ -10,6 +10,8 @@ import static com.majorperk.marketservice.utils.Constants.BRAND_OBJECT_STATUS;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -19,22 +21,21 @@ import com.majorperk.marketservice.model.reward.SmallBrand;
 
 @Repository
 public interface BrandRepository extends JpaRepository<Brand, Long> {
-    List<Brand> findAll();
+	List<Brand> findAll();
 
-    Brand findOneByBrandKey();
+	String FIND_ALL_CONDENSED = "SELECT new " + "com.majorperk.marketservice.model.reward.SmallBrand" + "("
+			+ BRAND_OBJECT_ID + ", " + BRAND_OBJECT_KEY + ", " + BRAND_OBJECT_NAME + ", " + BRAND_OBJECT_DESC_SHORT
+			+ ", " + BRAND_OBJECT_STATUS + ", " + BRAND_OBJECT_IMG_URLS + ") FROM " + BRAND_OBJECT
+			+ " WHERE status ='active'";
 
-    String FIND_ALL_CONDENSED = "SELECT new " +
-        "com.majorperk.marketservice.model.reward.SmallBrand" +
-        "(" + BRAND_OBJECT_ID + ", " + 
-            BRAND_OBJECT_KEY + ", " + 
-            BRAND_OBJECT_NAME + ", " + 
-            BRAND_OBJECT_DESC_SHORT + ", " + 
-            BRAND_OBJECT_STATUS + ", " + 
-            BRAND_OBJECT_IMG_URLS +
-        ") FROM " + BRAND_OBJECT +
-        " WHERE status ='active'";
+	// @Query("SELECT new com.majorperk.marketservice.model.reward.SmallBrand(id,
+	// brandKey, brandName, shortDescription, status, imageUrls) FROM Brand WHERE
+	// status='active'")
+	@Query(FIND_ALL_CONDENSED)
+	public List<SmallBrand> findAllCondensed();
 
-    // @Query("SELECT new com.majorperk.marketservice.model.reward.SmallBrand(id, brandKey, brandName, shortDescription, status, imageUrls) FROM Brand WHERE status='active'")
-    @Query(FIND_ALL_CONDENSED)
-    public List<SmallBrand> findAllCondensed();
+	@Valid
+	Brand findOneByBrandKey(String brandKey);
+
+	Brand findOneByBrandName(String brandName);
 }
